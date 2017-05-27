76345d9f80318b6607e1d26bb0c1ee1814c9f80f
GodJungGun <jg0428.lim@gmail.com>
2017-05-27 15:46:32 +0900

    home/index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,10 @@
 class HomeController < ApplicationController
+  def index
+    @home = Home.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render xml: @homes }
+    end
+  end
 end
